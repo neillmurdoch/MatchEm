b3eae212e40e6217e3d88603b4f0a2b8a55adc5c
neillmurdoch <neill.murdoch@gmail.com>
2017-08-23 20:54:27 +0100

    Switched to UI elements and shuffled folders

---
+++ Matchem/Assets/Scripts/Draggable.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    Transform originalParent = null;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.LogFormat("OnBeginDrag: {0}", eventData.position.ToString());
+
+        originalParent = this.transform.parent;
+
+        this.transform.SetParent(this.transform.parent.parent);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        this.transform.position = eventData.position;
+        Debug.Log("OnDrag");
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnEndDrag");
+        this.transform.SetParent(originalParent);
+    }
+}

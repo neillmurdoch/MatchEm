246d88ee4acc59fea990f4386e5d7b313a6dc92e
neillmurdoch <neill.murdoch@gmail.com>
2017-09-15 14:22:29 +0100

    Added symbol sprites. Got drag/drop mechanism to work

--- Matchem/Assets/Scripts/Draggable.cs
+++ Matchem/Assets/Scripts/Draggable.cs
@@ -5,7 +5,7 @@ using UnityEngine.EventSystems;
 public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     public Transform originalParent = null;
-    private int originalLayer = 0;
+    private int originalLayer = -1;
 
     public void OnBeginDrag(PointerEventData eventData)
     {
@@ -13,42 +13,26 @@ public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDra
 
         originalParent = this.transform.parent;
         originalLayer = gameObject.layer;
-
+    
         this.transform.SetParent(this.transform.root);
-        gameObject.layer = LayerMask.NameToLayer("Dragging");
 
-        //GetComponent<CanvasGroup>().blocksRaycasts = false;
-
-        //DropTarget[] targets = GameObject.FindObjectsOfType<DropTarget>();
-        //foreach (var target in targets)
-        //{
-        //    Renderer renderer = GetComponent<Renderer>();
-        //    renderer.material.color = Color.black;
-        //}
-
+        gameObject.layer = 2;   // Ignore raycast layer while moving.
+        
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        this.transform.position = eventData.position;
-        Debug.LogFormat("OnDrag: {0}, {1}, {2}", this.name, eventData.position.ToString(), LayerMask.LayerToName(gameObject.layer));
+        Vector2 pos = Camera.main.ScreenToWorldPoint(eventData.position);
+        this.transform.position = new Vector3(pos.x, pos.y, 0f);
+        //Debug.LogFormat("OnDrag: {0}, {1}, {2}, {3}", this.name, eventData.position.ToString(), LayerMask.LayerToName(gameObject.layer), this.transform.position.ToString());
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
         this.transform.SetParent(originalParent);
         gameObject.layer = originalLayer;
-
+        
         Debug.LogFormat("OnEndDrag: {0}, {1}, {2}", this.name, eventData.position.ToString(), LayerMask.LayerToName(gameObject.layer));
-
-        //GetComponent<CanvasGroup>().blocksRaycasts = true;
-
-        //DropTarget[] targets = GameObject.FindObjectsOfType<DropTarget>();
-        //foreach (var target in targets)
-        //{
-        //    Renderer renderer = GetComponent<Renderer>();
-        //    renderer.material.color = Color.white;
-        //}
-
+        
     }
 }
